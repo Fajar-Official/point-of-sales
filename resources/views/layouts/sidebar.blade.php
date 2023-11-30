a0cd30fc3df0886bf36333ec95d7e3f906346c14
Muhammad Fajar Ramadhan <fajarmrtnet@gmail.com>
2023-11-30 19:17:54 +0800

    Integrasi Admin LTE

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,120 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                    Dashboard
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-header">Master</li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    Kategori
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-image"></i>
+                <p>
+                    Product
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Member
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Supplier
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-header">Transaksi</li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    Pengeluaran
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-image"></i>
+                <p>
+                    Pembelian
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Penjualan
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Transaksi Lama
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Transaksi Baru
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-header">Report</li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    Laporan
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-header">Sistem</li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    User
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-image"></i>
+                <p>
+                    Pengaturan
+                </p>
+            </a>
+        </li>
+
+    </ul>
+</nav>
+<!-- /.sidebar-menu -->
