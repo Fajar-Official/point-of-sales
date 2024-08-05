30b6c2bd4dc83b48cc57eedb2c83071b43efdf2b
Muhammad Fajar Ramadhan <60921998+Fajar-Official@users.noreply.github.com>
2024-08-05 20:19:39 +0800

    feat : dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
@@ -25,7 +24,13 @@ createInertiaApp({
             });
 
             return hasPermission;
-          }
+          },
+
+          //format price
+          formatPrice(value) {
+            let val = (value/1).toFixed(0).replace('.', ',')
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+          },
 
         },
       })
